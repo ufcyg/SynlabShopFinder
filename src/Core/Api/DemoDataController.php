a564ee8331cd71e115aa1067dc54db0281a4b04a
ufcyg <mellowski@gmail.com>
2020-10-11 12:28:54 +0200

    added namespace to controller

--- src/Core/Api/DemoDataController.php
+++ src/Core/Api/DemoDataController.php
@@ -1,5 +1,7 @@
 <?php declare(strict_types=1);
 
+namespace SynlabShopFinder\Core\Api;
+
 use Faker\Factory;
 use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
@@ -13,7 +15,6 @@ use Shopware\Core\System\Country\CountryEntity;
 use Shopware\Core\System\Country\Exception\CountryNotFoundException;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Routing\Annotation;
 
 /**
  * @RouteScope(scopes={"api"})
